9c36928a31e29a805b2faeeef03b13a6b3a3657d
Benedikt Geib <benedikt.geib@googlemail.com>
2023-01-15 12:10:47 +0100

    switch to rumqttc

--- src/config.rs
+++ src/config.rs
@@ -3,8 +3,10 @@ use serde::{Deserialize, Serialize};
 
  #[derive(Serialize, Deserialize, Debug)]
  pub struct ConfigData {
-    pub broker_url: String,
-    pub topic_temperature: String,
-    pub topic_humidity: String,
-    pub dev_hdc1008: String,
+   pub broker_url: String,
+   pub broker_port: u16,
+   pub broker_conn_timeout: u64,
+   pub topic_temperature: String,
+   pub topic_humidity: String,
+   pub dev_hdc1008: String,
  }
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,21 @@
 extern crate i2cdev;
-extern crate paho_mqtt as mqtt;
-
 pub mod config;
-
-use std::env;
-use std::fs::File;
-use std::io::BufReader;
-use std::process;
-use serde_json;
-use std::thread;
-use std::time::Duration;
 
 use i2cdev::core::*;
 use i2cdev::linux::{LinuxI2CDevice, LinuxI2CError};
+use rumqttc::{MqttOptions, Client, QoS};
+use serde_json;
+use std::fs::File;
+use std::io::BufReader;
+use std::thread;
+use std::time::Duration;
+
 
 use crate::config::ConfigData;
 
 const NUNCHUCK_SLAVE_ADDR: u16 = 0x40;
 const DFLT_CLIENT:&str = "local_tempHumidity";
-const QOS:i32 = 0;
+const QOS:QoS = QoS::AtMostOnce;
 const SLEEPTIME:u64 = 60;
 
 fn main() {
@@ -26,42 +23,24 @@ fn main() {
 }
 
 fn i2cfun() -> Result<(), LinuxI2CError> {
-    let file = File::open("./Config_Hdc10082Mqtt.json")?;
+    let file = File::open("config/Hdc10082Mqtt.json")?;
     let reader = BufReader::new(file);
     let config: ConfigData = serde_json::from_reader(reader).expect("error while reading or parsing");
     let mut dev = LinuxI2CDevice::new(config.dev_hdc1008, NUNCHUCK_SLAVE_ADDR)?;
 
     // init sequence
     dev.smbus_write_word_data(0x02, 0x0090)?;
-    
-    let host = env::args().nth(1).unwrap_or_else(||
-        config.broker_url
-    );
 
-    // Define the set of options for the create.
-    // Use an ID for a persistent session.
-    let create_opts = mqtt::CreateOptionsBuilder::new()
-        .server_uri(host)
-        .client_id(DFLT_CLIENT.to_string())
-        .finalize();
+    //create mqtt client and connect
+    let mut mqttoptions = MqttOptions::new(DFLT_CLIENT.to_string(), config.broker_url, config.broker_port);
+    mqttoptions.set_keep_alive(Duration::from_secs(config.broker_conn_timeout));
 
-    // Create a client.
-    let cli = mqtt::Client::new(create_opts).unwrap_or_else(|err| {
-        println!("Error creating the client: {:?}", err);
-        process::exit(1);
+    let (mut client, mut connection) = Client::new(mqttoptions, 10);
+
+    thread::spawn(move || for (_i, notification) in connection.iter().enumerate() {
+        println!("Notification = {:?}", notification);
+        thread::sleep(Duration::from_millis(1000));
     });
-
-    // Define the set of options for the connection.
-    let conn_opts = mqtt::ConnectOptionsBuilder::new()
-        .keep_alive_interval(Duration::from_secs(SLEEPTIME*2))
-        .clean_session(true)
-        .finalize();
-
-    // Connect and wait for it to complete or fail.
-    if let Err(e) = cli.connect(conn_opts) {
-        println!("Unable to connect:\n\t{:?}", e);
-        process::exit(1);
-    }
 
     loop {
         let mut buf: [u8; 4] = [0; 4];
@@ -73,16 +52,14 @@ fn i2cfun() -> Result<(), LinuxI2CError> {
         let humid = ((buf[2] as u16 * 256 + buf[3] as u16) as f64)/65536.0 * 100.0;
         println!("temp {:?} °C humid {:?} %", temp, humid);
         {
-            let msg = mqtt::Message::new_retained(config.topic_humidity.as_str(), humid.to_string(), QOS);
-            let tok = cli.publish(msg);
+            let tok = client.publish(config.topic_humidity.as_str(), QOS, true, humid.to_string());
 
             if let Err(e) = tok {
                 println!("Error sending message: {:?}", e);
                 }
         }
         {
-            let msg = mqtt::Message::new_retained(config.topic_temperature.as_str(), temp.to_string(), QOS);
-            let tok = cli.publish(msg);
+            let tok = client.publish(config.topic_temperature.as_str(), QOS, true, temp.to_string());
 
             if let Err(e) = tok {
                 println!("Error sending message: {:?}", e);
@@ -90,10 +67,4 @@ fn i2cfun() -> Result<(), LinuxI2CError> {
         }
         thread::sleep(Duration::from_secs(SLEEPTIME));
     }
-
-
-    // Disconnect from the broker.
-    let tok = cli.disconnect(None);
-    println!("Disconnect from the broker");
-    tok.unwrap();
 }
